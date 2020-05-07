af934d83b68c92f36b4a10e9af08051aebb0713d
whatislove118 <oleg.pochikalin@mail.ru>
2020-05-07 17:50:05 +0300

    lolkek

--- src/com/lab3/HibernateSessionFactory.java
+++ src/com/lab3/HibernateSessionFactory.java
@@ -15,7 +15,7 @@ public class HibernateSessionFactory {
 //                    .getCurrentInstance().getExternalContext().getContext();
 //            String deploymentDirectoryPath = ctx.getRealPath("/");
             sessionFactory = new Configuration()
-                    .configure(new File("web/WEB-INF/hibernate.cfg.xml"))
+                    .configure(new File("3LabJSF/web/WEB-INF/hibernate.cfg.xml"))
                     .buildSessionFactory();
         } catch (Throwable ex) {
             System.err.println("Initial SessionFactory creation failed." + ex);
